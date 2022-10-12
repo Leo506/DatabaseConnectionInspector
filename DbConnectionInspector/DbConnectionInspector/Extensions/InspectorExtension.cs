cbe7f681390f57baf3514b3ec940e674e4fa9ad0
Artem Levin <leoartemlev@yandex.ru>
2022-10-12 19:04:46 +0500

    Inspector Extension

---
+++ DbConnectionInspector/DbConnectionInspector/Extensions/InspectorExtension.cs
@@ -0,0 +1,15 @@
+﻿using DbConnectionInspector.Abstractions;
+using DbConnectionInspector.Core;
+using Microsoft.AspNetCore.Builder;
+
+namespace DbConnectionInspector.Extensions;
+
+public static class InspectorExtension
+{
+    public static IApplicationBuilder UseDbConnectionInspector(this IApplicationBuilder app,
+        params IDatabaseConnection[] connections)
+    {
+        app.UseMiddleware<Inspector>(connections);
+        return app;
+    }
+}
