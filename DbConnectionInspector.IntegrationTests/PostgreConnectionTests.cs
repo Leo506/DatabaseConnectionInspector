0a2180e75191c2d554fd7ed6aa26c4c0a65d6622
Artem Levin <leoartemlev@yandex.ru>
2022-10-12 18:01:33 +0500

    added postgresql connection checking

---
+++ DbConnectionInspector.IntegrationTests/PostgreConnectionTests.cs
@@ -0,0 +1,32 @@
+using DbConnectionInspector.Connections;
+
+namespace DbConnectionInspector.IntegrationTests;
+
+public class PostgreConnectionTests
+{
+    [Fact]
+    public async Task IsConnectionOpen_Connection_Open_Returns_True()
+    {
+        // arrange
+        var sut = new PostgreConnection("User ID=admin;Password=password;Host=localhost;Port=5432;Database=Test");
+
+        // act
+        var result = await sut.IsConnectionOpen();
+
+        // assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task IsConnectionOpen_Incorrect_Connection_String_Returns_False()
+    {
+        // arrange
+        var sut = new PostgreConnection("User ID=admin;Password=password;Host=localhost;Port=5433;Database=Test");
+
+        // act
+        var result = await sut.IsConnectionOpen();
+
+        // assert
+        Assert.False(result);
+    }
+}
---
+++ DbConnectionInspector/DbConnectionInspector/Connections/PostgreConnection.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using DbConnectionInspector.Abstractions;
+using Npgsql;
+
+namespace DbConnectionInspector.Connections;
+
+public class PostgreConnection : IDatabaseConnection
+{
+    private readonly string _connectionString;
+
+    public PostgreConnection(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task<bool> IsConnectionOpen()
+    {
+        try
+        {
+            await using var conn = new NpgsqlConnection(_connectionString);
+            await conn.OpenAsync();
+            return conn.State == ConnectionState.Open;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
