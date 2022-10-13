62034f5794490a3152353abbfa73fb26f99fd045
Artem Levin <leoartemlev@yandex.ru>
2022-10-13 22:33:34 +0500

    fix connection bug

--- DbConnectionInspector/DbConnectionInspector/Connections/PostgresConnection.cs
+++ DbConnectionInspector/DbConnectionInspector/Connections/PostgresConnection.cs
@@ -4,21 +4,45 @@ using Npgsql;
 
 namespace DbConnectionInspector.Connections;
 
-public class PostgresConnection : IDatabaseConnection
+public class PostgresConnection : IDatabaseConnection, IDisposable
 {
     public string? ConnectionString { get; set; }
+    private NpgsqlConnection? _connection = null;
+    private bool _isOpen = false;
 
     public async Task<bool> IsConnectionOpen()
     {
         try
         {
-            await using var conn = new NpgsqlConnection(ConnectionString);
-            await conn.OpenAsync();
-            return conn.State == ConnectionState.Open;
+
+            if (_connection == null)
+            {
+                _connection = new NpgsqlConnection(ConnectionString);
+                _connection.Settings.KeepAlive = 1;
+                _connection.StateChange += (sender, args) => _isOpen = _connection.FullState == ConnectionState.Open;
+
+                await _connection.OpenAsync();
+
+                _isOpen = _connection.FullState == ConnectionState.Open;
+            }
+
+            if (_isOpen == false)
+            {
+                await _connection.OpenAsync();
+                _isOpen = _connection.FullState == ConnectionState.Open;
+            }
+
+            return _isOpen;
+
         }
         catch (Exception)
         {
             return false;
         }
+    }
+
+    public void Dispose()
+    {
+        _connection?.Dispose();
     }
 }
