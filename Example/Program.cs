11a777fb9c1e1a61d6e3a97e93e3f41d00072c89
Artem Levin <leoartemlev@yandex.ru>
2022-10-14 22:53:12 +0500

    added example of how to use library

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,6 @@
 
 using System.Data;
+using System.Net;
 using DbConnectionInspector.Abstractions;
 using DbConnectionInspector.Connections;
 using DbConnectionInspector.Extensions;
@@ -30,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Here the common usage of library
+// but you can can use one of another ways
 app.UseDbConnectionInspector(new ConnectionOptions()
 {
     Connections = new []
@@ -40,6 +43,22 @@ app.UseDbConnectionInspector(new ConnectionOptions()
         }
     }
 });
+
+// You may not provide any connection instances
+// In this case checking would be always succeed
+/* app.UseDbConnectionInspector(new ConnectionOptions()); */
+
+// You may specify what action would be invoked if connection failed
+/*app.UseDbConnectionInspector(new ConnectionOptions()
+{
+    Connections = new []
+    {
+        new PostgresConnection()
+        {
+            ConnectionString = connString
+        }
+    }
+}, context => context.Response.StatusCode = (int)HttpStatusCode.BadRequest);*/
 
 app.UseHttpsRedirection();
 
