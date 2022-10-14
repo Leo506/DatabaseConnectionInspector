8099eebd1132801e131945abb1763e140d2788b0
Artem Levin <leoartemlev@yandex.ru>
2022-10-14 21:20:15 +0500

    added logger for creating Inspector

--- DbConnectionInspector/DbConnectionInspector/Extensions/InspectorExtension.cs
+++ DbConnectionInspector/DbConnectionInspector/Extensions/InspectorExtension.cs
@@ -3,6 +3,9 @@ using DbConnectionInspector.Abstractions;
 using DbConnectionInspector.Connections;
 using DbConnectionInspector.Core;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DbConnectionInspector.Extensions;
 
@@ -11,6 +14,14 @@ public static class InspectorExtension
     public static IApplicationBuilder UseDbConnectionInspector(this IApplicationBuilder app,
         ConnectionOptions options)
     {
-        return app.UseMiddleware<Inspector>(options);
+        var logger = app.ApplicationServices.GetRequiredService<ILogger<Inspector>>();
+        return app.UseMiddleware<Inspector>(options, logger);
+    }
+
+    public static IApplicationBuilder UseDbConnectionInspector(this IApplicationBuilder app, ConnectionOptions? options,
+        Action<HttpContext> action)
+    {
+        var logger = app.ApplicationServices.GetRequiredService<ILogger<Inspector>>();
+        return app.UseMiddleware<Inspector>(options, logger, action);
     }
 }
