456897a970e5cfc51a6d3a1321a2cc82027d3cca
Artem Levin <leoartemlev@yandex.ru>
2022-10-15 23:52:42 +0500

    class for checking connection

--- DbConnectionInspector.UnitTests/ConnectionCheckerTests.cs
+++ DbConnectionInspector.UnitTests/ConnectionCheckerTests.cs
@@ -8,17 +8,37 @@ namespace DbConnectionInspector.UnitTests;
 
 public class ConnectionCheckerTests
 {
-    [Fact]
-    public async Task IsConnectionEstablish_All_Good_Returns_True()
+    [Theory]
+    [InlineData(ConnectionState.Open, true)]
+    [InlineData(ConnectionState.Closed, false)]
+    public async Task IsConnectionEstablish_Check_State_Success(ConnectionState state, bool expected)
     {
         // arrange
         var connection = new Mock<DbConnection>();
-        connection.SetupGet(dbConnection => dbConnection.State).Returns(ConnectionState.Open);
+        connection.SetupGet(dbConnection => dbConnection.State).Returns(state);
         var sut = new ConnectionChecker(connection.Object);
 
         // act
         var result = await sut.IsConnectionEstablish();
         
+        // assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public async Task IsConnectionEstablish_Should_First_Open_Connection_Success()
+    {
+        // arrange
+        var connection = new Mock<DbConnection>();
+        connection.SetupGet(dbConnection => dbConnection.State).Returns(ConnectionState.Closed);
+        connection.Setup(dbConnection => dbConnection.Open()).Callback(() =>
+            connection.SetupGet(dbConnection => dbConnection.State).Returns(ConnectionState.Open));
+
+        var sut = new ConnectionChecker(connection.Object);
+        
+        // act
+        var result = await sut.IsConnectionEstablish();
+
         // assert
         result.Should().Be(true);
     }
--- DbConnectionInspector/DbConnectionInspector/Connections/ConnectionChecker.cs
+++ DbConnectionInspector/DbConnectionInspector/Connections/ConnectionChecker.cs
@@ -4,17 +4,34 @@ using DbConnectionInspector.Abstractions;
 
 namespace DbConnectionInspector.Connections;
 
-public class ConnectionChecker : IConnectionChecker
+public class ConnectionChecker : IConnectionChecker, IDisposable
 {
-    private readonly DbConnection _connection;
+    private readonly DbConnection _connection = null!;
+    private bool _isEstablish;
 
     public ConnectionChecker(DbConnection connection)
     {
-        _connection = connection;
+        try
+        {
+            _connection = connection;
+            _connection.StateChange += (sender, args) => _isEstablish = _connection.State == ConnectionState.Open;
+            _connection.Open();
+            _isEstablish = _connection.State == ConnectionState.Open;
+        }
+        catch
+        {
+            _isEstablish = false;
+        }
     }
 
     public Task<bool> IsConnectionEstablish()
     {
-        return Task.FromResult(_connection.State == ConnectionState.Open);
+        return Task.FromResult(_isEstablish);
+    }
+
+    public void Dispose()
+    {
+        _connection.Close();
+        _connection.Dispose();
     }
 }
